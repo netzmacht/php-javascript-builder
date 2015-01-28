9e82fbba42eb45058dbc36136c2d2af88174fb74
David Molineus <david.molineus@netzmacht.de>
2015-01-28 10:46:19 +0100

    Add spec for flags class.

---
+++ spec/Netzmacht/JavascriptBuilder/FlagsSpec.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace spec\Netzmacht\JavascriptBuilder;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class FlagsSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Netzmacht\JavascriptBuilder\Flags');
+    }
+
+    function it_adds_a_flag()
+    {
+        $this->add(1, 2)->shouldReturn(3);
+        $this->add(1, 1)->shouldReturn(1);
+    }
+
+    function it_removes_a_flag()
+    {
+        $this->remove(1, 3)->shouldReturn(2);
+        $this->remove(1, 1)->shouldReturn(0);
+    }
+
+    function it_checks_if_flag_is_contained()
+    {
+        $this->contains(1, 3)->shouldReturn(true);
+        $this->contains(1, 2)->shouldReturn(false);
+    }
+}
