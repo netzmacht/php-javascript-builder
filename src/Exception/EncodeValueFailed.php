6eab7f70ba66773c2bec70edef8f791936a2e05c
David Molineus <david.molineus@netzmacht.de>
2019-06-07 15:16:26 +0200

    Update file headers.

--- src/Exception/EncodeValueFailed.php
+++ src/Exception/EncodeValueFailed.php
@@ -1,12 +1,13 @@
 <?php
 
 /**
- * @package    dev
- * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2014 netzmacht creative David Molineus
- * @license    LGPL 3.0
- * @filesource
+ * PHP Javascript Builder
  *
+ * @package    netzmacht/php-javascript-builder
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2014-2019 netzmacht David Molineus
+ * @license    LGPL-3.0-or-later https://github.com/netzmacht/php-javascript-builder/blob/master/LICENSE
+ * @filesource
  */
 
 namespace Netzmacht\JavascriptBuilder\Exception;
